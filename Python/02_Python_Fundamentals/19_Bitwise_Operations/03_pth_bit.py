2b89b5b2209da26f296d426ab3aa1e662a133620
Kalina Ilieva Raycheva <84627307+KalinaRaycheva@users.noreply.github.com>
2022-08-04 19:27:57 +0300

    Delete Python/02_Python_Fundamentals/19_Bitwise_Operations directory

--- Python/02_Python_Fundamentals/19_Bitwise_Operations/03_pth_bit.py
+++
@@ -1,6 +0,0 @@
-number = int(input())
-position = int(input())
-
-result = (number >> position) & 1
-
-print(result)
--- Python/02_Python_Fundamentals/19_Bitwise_Operations/04_bit_destroyer.py
+++
@@ -1,7 +0,0 @@
-number = int(input())
-position = int(input())
-
-mask = ~(1 << position)
-result = mask & number
-
-print(result)
