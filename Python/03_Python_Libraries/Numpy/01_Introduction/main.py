371c44740cd71d66cf0b651fc4d503656f5953d4
Kalina Ilieva Raycheva <84627307+KalinaRaycheva@users.noreply.github.com>
2022-12-07 00:06:05 +0200

    fixing little errors
    removing unused brackets

--- Python/03_Python_Libraries/Numpy/01_Introduction/main.py
+++ Python/03_Python_Libraries/Numpy/01_Introduction/main.py
@@ -11,8 +11,8 @@ print("Initial Array:\n", arr)
 sliced_arr = arr[:2, ::2]
 print("Sliced array with first 2 rows and columns 0 and 2:\n", sliced_arr)
 
-index_arr = arr[[
+index_arr = arr[
                 [1, 1, 0, 3],
                 [3, 2, 1, 0],
-                ]]
+                ]
 print("\nElements at indices (1, 3), (1, 2), (0, 1), (3, 0):\n", index_arr)
