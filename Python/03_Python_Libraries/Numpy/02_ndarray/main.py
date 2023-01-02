bab09e48d4426fde7027412f5aa64b9b416c8de7
Kalina Ilieva Raycheva <84627307+KalinaRaycheva@users.noreply.github.com>
2023-01-02 23:23:33 +0200

    Update 02_ndarray
    Adding tries

--- Python/03_Python_Libraries/Numpy/02_ndarray/main.py
+++ Python/03_Python_Libraries/Numpy/02_ndarray/main.py
@@ -9,3 +9,19 @@ print("No. of dimensions: ", arr.ndim)
 print("Shape of array: ", arr.shape)
 print("Size of array: ", arr.size)
 print("Array stores elements of type: ", arr.dtype)
+
+#----------------------------------------------------------------------------
+
+
+a = np.array([[1, 2, 4], [5, 8, 7]], dtype = 'float')
+print ("Array created using passed list:\n", a)
+ 
+b = np.array((1 , 3, 2))
+print ("\nArray created using passed tuple:\n", b)
+ 
+c = np.zeros((3, 4))
+print ("\nAn array initialized with all zeros:\n", c)
+
+d = np.full((3, 3), 6, dtype = 'complex')
+print ("\nAn array initialized with all 6s."
+            "Array type is complex:\n", d)
