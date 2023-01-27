6490dd20771366764e12de1a6f953536e0bbc9bb
Kalina Ilieva Raycheva <84627307+KalinaRaycheva@users.noreply.github.com>
2023-01-27 23:50:47 +0200

    Add 02_queue_using_deque.py

---
+++ Python/02_Courses_in_CodeAcademy/12_stack_and_queue/02_queue_using_deque.py
@@ -0,0 +1,20 @@
+from collections import deque
+from queue import Queue
+
+#first way
+q = deque()
+q.append('a')
+q.append('b')
+q.append('c')
+
+print(q.popleft())
+print(q)
+
+#second way
+q2 = Queue()
+q2.put('a')
+q2.put('b')
+q2.put('c')
+print(q2.qsize())
+print(q2.get())
+print(q2.qsize())
